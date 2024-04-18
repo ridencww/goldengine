73b49ddb8651f8a043906e35ce57d5456dc1b277
ridencww <riden@creativewidgetworks.com>
2024-04-18 13:02:52 -0500

    Removed target folder and files

--- simple2/target/classes/examples/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -classpath ..\..\. com.creativewidgetworks.goldparser.simple2.Simple2 %1 %2 %3 %4
--- simple3/target/test-classes/examples/run.bat
+++
@@ -1,2 +0,0 @@
-@echo off
-java -classpath ..\..\. com.creativewidgetworks.goldparser.simple3.Simple3 %1 %2 %3 %4
